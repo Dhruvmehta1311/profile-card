8f91357f54cdbc40c3f98d1d45c3eeb13cb0d9e9
Dhruv Mehta <flashxx001@gmail.com>
2024-07-07 12:58:52 +0530

    Created an array and rendered each skill using map.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,47 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
+const userInformation = [
+  {
+    id: 1,
+    name: "Dhruv",
+    desc: "Full-stack web developer and teacher at Udemy. When not coding or preparing a course, I like to play board games, to cook (and eat), or to just enjoy the Portuguese sun at the beach.",
+  },
+];
+
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "bg-yellow-500",
+  },
+  {
+    skill: "JavaScript",
+    level: "advanced",
+    color: "bg-red-500",
+  },
+  {
+    skill: "Web Design",
+    level: "advanced",
+    color: "bg-green-500",
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "bg-black",
+  },
+  {
+    skill: "React",
+    level: "advanced",
+    color: "bg-lime-600",
+  },
+  {
+    skill: "Svelte",
+    level: "beginner",
+    color: "bg-pink-500",
+  },
+];
+
 function App() {
   return (
     <>
@@ -14,40 +55,41 @@ function App() {
 function Skeleton() {
   return (
     <div className="bg-black h-screen flex items-center justify-center">
-      <div className="bg-white w-[480px] max-w-[90%] min-h-[700px] rounded-lg p-4 flex flex-col gap-4 items-center">
+      <div className="bg-white w-[480px] max-w-[90%] min-h-[560px] rounded-lg p-4 flex flex-col gap-4 items-center">
         <MainImage />
-        <UserInfo
-          name="Dhruv Mehta"
-          information="Full-stack web developer and teacher at
-Udemy. When not coding or preparing a
-course, I like to play board games, to
-cook (and eat), or to just enjoy the
-Portuguese sun at the beach."
-        />
-        <div className="flex flex-wrap p-4 gap-2">
-          <UserSkills skill="HTML + CSS" emoji="💪" color="bg-red-600" />
-          <UserSkills skill="JavaScript" emoji="💪" color="bg-blue-600" />
-          <UserSkills skill="Web Design" emoji="💪" color="bg-green-600" />
-          <UserSkills skill="Git and GitHub" emoji="👍" color="bg-yellow-600" />
-          <UserSkills skill="Svelte" emoji="🐣" color="bg-purple-600" />
+
+        {userInformation.map((singleUser) => (
+          <UserInfo userDataObj={singleUser} key={singleUser.id} />
+        ))}
+
+        <div className="flex flex-wrap p-4">
+          {skills.map((skill) => (
+            <UserSkills skillsObj={skill} key={skill.skill} />
+          ))}
         </div>
       </div>
     </div>
   );
 }
-function UserInfo(props) {
+function UserInfo({ userDataObj }) {
+  console.log(userDataObj);
   return (
     <div className="p-4">
-      <h1 className="font-bold text-2xl text-center">{props.name}</h1>
-      <p className="text-center">{props.information}</p>
+      <h1 className="font-bold text-2xl text-center">{userDataObj.name}</h1>
+      <p className="text-center">{userDataObj.desc}</p>
     </div>
   );
 }
 
-function UserSkills(props) {
+function UserSkills({ skillsObj }) {
+  console.log(skillsObj);
   return (
-    <button className={`mt-4 ${props.color} px-4 rounded-md py-4 text-white`}>
-      {props.skill} {props.emoji}
+    <button
+      className={`mt-4 ${skillsObj.color} px-4 rounded-md mr-4 py-4 text-white`}
+    >
+      {skillsObj.skill} {skillsObj.level === "advanced" ? "💪" : ""}{" "}
+      {skillsObj.level === "intermediate" ? "👍" : ""}
+      {skillsObj.level === "beginner" ? "🐣" : ""}
     </button>
   );
 }
